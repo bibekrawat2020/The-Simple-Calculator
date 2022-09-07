6244e7a4c8d334850b9393074e91f4dfb5d15839
Bibek Rawat <bibekrawat14@gmail.com>
2022-09-07 20:22:26 +0545

    Added user input for option

--- Program.cs
+++ Program.cs
@@ -8,3 +8,11 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter a second number and press enter");
 int num2 = Convert.ToInt32(Console.ReadLine());
+
+//Taking input for the option from the user
+Console.WriteLine("Choose an option for performing following operation:");
+Console.WriteLine("\ta - Add");
+Console.WriteLine("\ts - Subtract");
+Console.WriteLine("\tm - Multiply");
+Console.WriteLine("\td - Divide");
+Console.Write("Your option? ");
