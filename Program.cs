13f228bafdf0cf05446ea84c5b502781f0a9b6f3
Bibek Rawat <bibekrawat14@gmail.com>
2022-09-07 20:12:38 +0545

    Added User Input

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿// Project Initializer
 Console.WriteLine("Calculator");
 Console.WriteLine("----------\n");
+
+//Taking input from the user
+Console.WriteLine("Enter a first number and press enter");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a second number and press enter");
+int num2 = Convert.ToInt32(Console.ReadLine());
